8dda3f2c574c62a5b0eec59b909faf1fcd549e3c
athenaz14 <athenaz11@nycstudents.net>
2026-04-13 09:26:10 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,10 +8,7 @@ print(data[0])
 # Create a function that will take the data from the JSON file and you will iterate through the list of pokemon and print each pokemons name.
 
 # Add a language choice feature and print the pokemons name based on the user input
-user_input =input("Choose the language for pokemons name: English, Japanese, Chinese, and French")
 
-if user_input == English:
-        print
 # Develop a function that creates a new list of pokemon based on the type the user searched for. If no pokemon was found of that type inform the user
 
 #Develop a function to find all pokemon matching the name the user searched for. Ex. if "Char" return Charmander, Charmeleon and Charizard. Make the user aware if no pokemon was found. 
