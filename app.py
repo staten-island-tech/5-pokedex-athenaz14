6f2ed31153314494e122abc2d4eb21063470b37c
athenaz14 <athenaz11@nycstudents.net>
2026-04-15 09:26:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,23 +5,34 @@ pokedex = open("./pokedex.json", encoding="utf8")
 data = json.load(pokedex)
 print(data[0])
 # Create a function that will take the data from the JSON file and you will iterate through the list of pokemon and print each pokemons name.
-names = {}
-
-
-# Add a language choice feature and print the pokemons name based on the user input
-""" Language = input("Enter a language: English, French, Chinese, Japanese")
-if Language == "English":
-
-elif Language == "French":
-
-elif Language == "Chinese":
-
-else:
+""" names = {}
  """
+for index, pokemon in enumerate(data): 
+    print(index, ":",pokemon["name"])
 
+""" def pokemon_names(json):
+    for pokemon_name in pokemon_names:
+        if pokemon_name in items.json:
+ """
+# Add a language choice feature and print the pokemons name based on the user input
+Language = input("Enter a language: English, French, Chinese, Japanese")
+if Language == "English":
+    for index, pokemon in enumerate(data): 
+        print(pokemon["name"]["english"])
+elif Language == "French":
+    for index, pokemon in enumerate(data): 
+        print(pokemon["name"]["french"])
+elif Language == "Chinese":
+    for index, pokemon in enumerate(data): 
+        print(pokemon["name"]["chinese"])
+else:
+    for index, pokemon in enumerate(data): 
+        print(pokemon["name"]["japanese"])
 
 # Develop a function that creates a new list of pokemon based on the type the user searched for. If no pokemon was found of that type inform the user
-
+Type = input("Enter a type of pokemon:")
+if pokemon["type"] in data:
+    print(pokemon["type"])
 #Develop a function to find all pokemon matching the name the user searched for. Ex. if "Char" return Charmander, Charmeleon and Charizard. Make the user aware if no pokemon was found. 
 
 #For Leo/, help me come up with a clever final question, considering maybe showing all moves a pokemon has avaiable based on type
