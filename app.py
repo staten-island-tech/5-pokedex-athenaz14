c0f81196c73cdc1795e22efb53d9372db952b3b8
athenaz14 <athenaz11@nycstudents.net>
2026-04-16 09:25:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -15,25 +15,23 @@ for index, pokemon in enumerate(data):
         if pokemon_name in items.json:
  """
 # Add a language choice feature and print the pokemons name based on the user input
-""" Language = input("Enter a language: English, French, Chinese, Japanese")
+Language = input("Enter a language: English, French, Chinese, Japanese")
 if Language == "English":
-    for index, pokemon in enumerate(data): 
         print(pokemon["name"]["english"])
 elif Language == "French":
-    for index, pokemon in enumerate(data): 
         print(pokemon["name"]["french"])
 elif Language == "Chinese":
-    for index, pokemon in enumerate(data): 
         print(pokemon["name"]["chinese"])
 else:
-    for index, pokemon in enumerate(data): 
         print(pokemon["name"]["japanese"])
- """
-# Develop a function that creates a new list of pokemon based on the type the user searched for. If no pokemon was found of that type inform the user
-Type = int(input("Enter a type of pokemon:"))
-print(pokemon[Type])
-#Develop a function to find all pokemon matching the name the user searched for. Ex. if "Char" return Charmander, Charmeleon and Charizard. Make the user aware if no pokemon was found. 
 
+# Develop a function that creates a new list of pokemon based on the type the user searched for. If no pokemon was found of that type inform the user
+Type = (input("Enter a type of pokemon:"))
+
+#Develop a function to find all pokemon matching the name the user searched for. Ex. if "Char" return Charmander, Charmeleon and Charizard. Make the user aware if no pokemon was found. 
+""" Search_Match = (input("All the names of the pokemon matching what the user searched for:"))
+ """
 #For Leo/, help me come up with a clever final question, considering maybe showing all moves a pokemon has avaiable based on type
 
 #Based on user input, show all moves that pokemon  could learn based on type. For example, if Charizard is fire/fyling, show all fire and flying moves.
+""" Moves = (input("Enter a pokemon to learn all the moves could learn base on type.")) """
